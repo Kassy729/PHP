dcbfdfd450780998e4d3988359c50beaa6430078
Kassy729 <hamtol98@gmail.com>
2021-10-06 16:31:18 +0900

    blade-post-commit

--- Laravel/demo6-app/resources/views/components/post-index.blade.php
+++ Laravel/demo6-app/resources/views/components/post-index.blade.php
@@ -1,22 +1,9 @@
-<div>
-    <table class="table table-hover mt-5">
-        <thead>
-          <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Writer</th>
-            <th scope="col">Create</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($posts as $post)
-          <tr>
-              <td><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a></td>
-              <td>{{ $post->user->name }}</td>
-              <td>{{ $post->updated_at->diffForHumans() }}</td>
-          </tr>
 
-          @endforeach
-        </tbody>
-      </table>
-      {{ $posts->links() }}
-</div>
+@foreach ($posts as $post)
+<tr>
+    <td><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a></td>
+    <td>{{ $post->user->name }}</td>
+    <td>{{ $post->updated_at->diffForHumans() }}</td>
+</tr>
+
+@endforeach
