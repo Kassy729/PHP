e95cb504fb049b8a048179ce00d9f0c680779368
Kassy729 <hamtol98@gmail.com>
2022-01-13 23:36:54 +0900

    commit

--- Laravel/auth-app/auth-api/app/Http/Controllers/AuthController.php
+++ Laravel/auth-app/auth-api/app/Http/Controllers/AuthController.php
@@ -5,45 +5,51 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
-
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
+        return User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
         ]);
-        return $user;
     }
 
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
-        if (!Auth::attempt($credentials)) {
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response([
-                'message' => '인증되지 않은 사용자 입니다'
-            ]);
-        };
+                'message' => 'Invalid credentials!'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
 
         $user = Auth::user();
 
-        $token = $user->createToken('access-token')->plainTextToken;
+        $token = $user->createToken('token')->plainTextToken;
 
-        // $cookie = cookie('jwt', $token, 60 * 24);
+        $cookie = cookie('jwt', $token, 60 * 24); // 1 day
 
-        $response = [
-            'user' => $user,
-            'token' => $token
-        ];
-        return response($response, 201);
+        return response([
+            'message' => $token
+        ])->withCookie($cookie);
     }
 
     public function user()
     {
         return Auth::user();
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+
+        return response([
+            'message' => 'Success'
+        ])->withCookie($cookie);
     }
 }
--- Laravel/auth-app/auth-api/routes/api.php
+++ Laravel/auth-app/auth-api/routes/api.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Route::get('/user', [AuthController::class, 'user']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
